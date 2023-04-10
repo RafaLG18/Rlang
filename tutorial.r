5d2a23cf686df0e17c815778c03cebf1f93a8d00
rafael <rafael.leal@pop-pe.rnp.br>
2023-04-10 08:57:29 -0300

    #1: adding initial files

---
+++ tutorial.r
@@ -0,0 +1,25 @@
+for (x in 1:10) {
+   print(x)
+}
+
+##Declaring Variables
+# numeric
+age <- 40
+# character/string
+name <- "John"
+# integer
+x <- 1000L
+# complex
+y <- 9i + 3
+# logical/boolean
+z <- TRUE
+
+class()
+
+temp <- "His name is"
+
+var1 <- var2 <- var3 <- "Orange"
+#Printing methods
+print(name)
+paste("His name is",name)
+
